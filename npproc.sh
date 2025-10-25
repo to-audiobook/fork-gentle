b6110bedfba9f7eb0d961166a4bc58920296a247
to-audiobook <null@null.com>
2025-10-25 18:05:42 +0000

    missing  shbang

--- npproc.sh
+++ npproc.sh
@@ -1,2 +1,4 @@
+#!/bin/sh
+
 # return the number of physical cores without relying on the existence of lscpu
 grep -E 'core id|physical id' /proc/cpuinfo | tr -d '\n' | sed 's;physical;\nphysical;g' | grep -v '^$' | sort -u | wc -l
